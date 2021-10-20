8952258bd4d46c0fed04de6b511326e6ea1b1627
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-10-20 16:29:46 +0530

    Added:employePyrollApp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ import Profile from "./profile";
+
+class APP extends Component{
+  render(){
+    return (
+      <h1>
+     {/* <Profile text ="hello props"/> */}
+     </h1>
+    );
+  }
+}
+
+export default App;
